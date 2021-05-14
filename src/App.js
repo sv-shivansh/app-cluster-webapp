071bd3b855e5ebe1811035d867082bb7b6fe5b8f
sv-shivansh <sv_shivansh@outlook.com>
2021-05-14 21:01:19 +0530

    stopwatch integrated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import Stopwatch from "./components/application/Stopwatch/Stopwatch";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <Switch>
+          <section className="container">
+            <Route exact path="/stopwatch" component={Stopwatch} />
+          </section>
+        </Switch>
+      </Fragment>
+    </Router>
   );
 }
 
---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+    return(
+        <header>
+            <div className= 'logo'><Link to='/'>appCluster</Link></div>
+        <nav>
+            <ul className= 'nav-links'>
+                <li><button><Link to = '/stopwatch'>Stopwatch</Link></button></li>
+                <li><button><Link to = '#'>Sorting Visualizer</Link></button></li>
+                {/* <li><button><Link to = "#"> More </Link></button></li>             */}
+            </ul>           
+        </nav>
+        </header>
+    )
+}
+
+export default Navbar
